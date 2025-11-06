d6f87bb88cb75d118a794864bca6c6568603be98
Fightarr <unmannedmediaplex@gmail.com>
2025-11-05 23:49:42 -0500

    fix: Add database migration to fix SABnzbd/NZBGet enum values
    This migration automatically fixes download clients that were saved with incorrect Type values due to the enum mapping bug.
    
    Changes:
    - Updates SABnzbd clients from Type=4 to Type=5
    - Updates NZBGet clients from Type=5 to Type=6
    
    This will run automatically on next app startup and fix the database permanently.
    
    Users no longer need to manually delete and re-add their clients.

---
+++ src/Migrations/20251106044904_FixDownloadClientEnumValues.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fightarr.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDownloadClientEnumValues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Fix SABnzbd clients that were saved with Type=4 (should be 5)
+            // This bug occurred because the frontend was missing UTorrent in the enum mapping
+            migrationBuilder.Sql(@"
+                UPDATE DownloadClients
+                SET Type = 5
+                WHERE Type = 4
+                AND (Name LIKE '%SAB%' OR Name LIKE '%sabnzbd%')
+            ");
+
+            // Fix NZBGet clients that were saved with Type=5 (should be 6)
+            migrationBuilder.Sql(@"
+                UPDATE DownloadClients
+                SET Type = 6
+                WHERE Type = 5
+                AND (Name LIKE '%NZB%' OR Name LIKE '%nzbget%')
+                AND NOT (Name LIKE '%SAB%')
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
