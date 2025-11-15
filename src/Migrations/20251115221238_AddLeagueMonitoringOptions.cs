40be1b863325f000227e5709f97e409c460196b2
Fightarr <unmannedmediaplex@gmail.com>
2025-11-15 17:19:07 -0500

    chore: Add database migration for league monitoring options
    Adds MonitorType, SearchForMissingEvents, and SearchForCutoffUnmetEvents columns to Leagues table.

---
+++ src/Migrations/20251115221238_AddLeagueMonitoringOptions.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sportarr.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLeagueMonitoringOptions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MonitorType",
+                table: "Leagues",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SearchForCutoffUnmetEvents",
+                table: "Leagues",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SearchForMissingEvents",
+                table: "Leagues",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MonitorType",
+                table: "Leagues");
+
+            migrationBuilder.DropColumn(
+                name: "SearchForCutoffUnmetEvents",
+                table: "Leagues");
+
+            migrationBuilder.DropColumn(
+                name: "SearchForMissingEvents",
+                table: "Leagues");
+        }
+    }
+}
